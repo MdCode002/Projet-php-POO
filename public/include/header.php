de855a7d52f2d03aee9756363df50aaeab80ccda
Mouhamed Diouf <111018812+Medzo02@users.noreply.github.com>
2023-06-06 18:41:04 +0000

    Delete public directory

--- public/include/header.php
+++
@@ -1,3 +0,0 @@
-<div class="Menu">
-    <img src="./../../public/img/logo.png" alt="" height="48px" style="margin-left:15px;margin-top:2px">
-</div>
--- public/script/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#parametre").addEventListener("click", () => {
-  document.querySelector(".blury").classList.toggle("togglemenu");
-  console.log("djkkk");
-});
-document.querySelector("#menu").addEventListener("click", () => {
-  document.querySelector(".blury").classList.toggle("togglemenu");
-  console.log("djkkk");
-});
